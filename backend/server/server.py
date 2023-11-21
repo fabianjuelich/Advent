ac7482f9a5c4b642895c928cb4ae80782fa24aa5
fabianjuelich <63878623+fabianjuelich@users.noreply.github.com>
2023-11-21 23:28:37 +0100

    Remove mailing list file from repo

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,9 +1,17 @@
 import os, xmlrpc.server, csv
 
+subs_csv = os.path.join(os.path.dirname(__file__), '../data/subscriptions.csv')
+
+def createCsv():
+    if not os.path.isfile(subs_csv):
+        with open(subs_csv, 'w+') as subs:
+            writer = csv.writer(subs)
+            writer.writerow(('email', 'number', 'onlyOnWin'))
+
 class Server:
     def subscribe(self, email, number, onlyOnWin):
         # append to mailing list
-        with open(os.path.join(os.path.dirname(__file__), '../data/subscriptions.csv'), 'a') as subs:
+        with open(subs_csv, 'a') as subs:
             writer = csv.writer(subs)
             writer.writerow((email, number, int(onlyOnWin)))
         # feedback
@@ -15,4 +23,5 @@ def serve():
     server.serve_forever()
 
 if __name__ == "__main__":
+    createCsv()
     serve()
