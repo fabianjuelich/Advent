04492dafb331291029d09ae5e35961ed1d24217b
fabianjuelich <63878623+fabianjuelich@users.noreply.github.com>
2023-12-01 15:27:13 +0000

    Log cronjob

--- backend/cronjob.py
+++ backend/cronjob.py
@@ -1,6 +1,16 @@
 from scraper import scraper
 from mail import mail
 from db import db
+import logging, os
+
+# log
+logging.basicConfig(filename=os.path.join(os.path.dirname(__file__), './logs/cronjob.log'),
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+logging.info(__file__)
 
 # read mailing list
 def subscribers():
@@ -11,10 +21,13 @@ def subscribers():
 def sendNotifications(subscribers):
     for email, number, daily in subscribers:
         # get winning status
-        result = scraper.scrape(number)
-        if result or int(daily):
-            # send email
-            mail.notify(email, number, result)
+        try:
+            result = scraper.scrape(number)
+            if result or int(daily):
+                # send email
+                mail.notify(email, number, result)
+        except Exception as e:
+            logging.error(e)
 
 def main():
     sendNotifications(subscribers())
