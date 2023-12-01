ec9c9423c304da171c186b1b018fadcedd6538c6
fabianjuelich <63878623+fabianjuelich@users.noreply.github.com>
2023-12-01 01:01:20 +0000

    Prototype scraper

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -1,2 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
 def scrape(number: int):
     return False
+
+page = 'http://www.lions-club-pulheim.de/adventskalender_gewinne.php'
+r = requests.get(page)
+soup = BeautifulSoup(r.text, 'html.parser')
+table = soup.find('table', class_ = 'adventtable')
+rows = table.find_all('tr')
+
+columns = []
+for row in rows:
+    for col in row.find_all('th'):
+        columns.append(col.text)
+        
+wins = []
+for row in rows:
+    data = {}
+    for col, val in zip(columns, row.find_all('td')):
+        data[col] = val.text
+    wins.append(data)
+
+for win in wins:
+    for key, value in win.items():
+        print(key, ':', value)
