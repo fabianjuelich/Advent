60f9c4aa318279e584fe1366e1682a17fbb76ea4
fabianjuelich <63878623+fabianjuelich@users.noreply.github.com>
2023-12-02 22:41:49 +0000

    Minor changes

--- backend/mail/mail.py
+++ backend/mail/mail.py
@@ -32,7 +32,7 @@ subject_lose = num + ' ☃️ Hinter diesem Türchen war leider nichts'
 quote = 'Wer nicht verlieren kann, verdient auch nicht zu gewinnen.'
 # won
 subject_win = num + ' 🎁 Hinter diesem Türchen war ein Gewinn'
-congrats = "Du hast {win} gewonnen."
+congrats = "Du hast {win} gewonnen. 🎉"
 
 # read content
 with open(os.path.join(os.path.dirname(__file__), './notification.txt'), 'r') as noteP, \
